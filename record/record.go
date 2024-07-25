38df9084e4467ba87a634c5d2180ff341a959407
wjunLu <wjunlu217@gmail.com>
2024-07-25 17:56:29 +0800

    update record.

--- record/record.go
+++ record/record.go
@@ -3,13 +3,41 @@ package record
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"path"
+	"time"
 
 	"github.com/eatmoreapple/openwechat"
 )
 
 const ROOT_DOR = "/tmp/ascend-bot/record/groups/"
+
+func RecordPictureMsg(msg *openwechat.Message) string {
+	if !msg.IsPicture() {
+		return msg.MsgId
+	}
+	// 创建文件
+	file, err := os.Create(path.Join(ROOT_DOR, "images/"+msg.MsgId+".jpg"))
+	if err != nil {
+		fmt.Println(err)
+		return msg.MsgId
+	}
+	defer file.Close()
+	// 将响应体写入文件
+	resp, err := msg.GetPicture()
+	if err != nil {
+		fmt.Println(err)
+		return msg.MsgId
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return file.Name()
+	}
+	return msg.MsgId
+}
 
 func RecordGroupChat(msg *openwechat.Message) {
 	user, err := msg.Sender()
@@ -24,15 +52,25 @@ func RecordGroupChat(msg *openwechat.Message) {
 		return
 	}
 	content := msg.Content
+	now := time.Now()
+	timeStr := now.Format("2006-01-02 15:04:05")
+	timeDayStr := now.Format("2006-01-02")
+	fmt.Println(timeStr, timeDayStr)
 	if !msg.IsText() {
-		content = "|> Non-Text <|"
+		content = RecordPictureMsg(msg)
 	}
 	conversation := GroupMsgRecord{
+		Time:    timeStr,
 		Sender:  sender.NickName,
 		Content: content,
 	}
 	// 当前消息存入文件，文件名为群名
-	WriteDataIntoFile(&conversation, path.Join(ROOT_DOR, group.NickName+".json"))
+	groupPath := path.Join(ROOT_DOR, group.NickName)
+	if err := os.MkdirAll(groupPath, 0755); err != nil {
+		fmt.Println(err)
+		return
+	}
+	WriteDataIntoFile(&conversation, path.Join(groupPath, timeDayStr+".json"))
 }
 
 func WriteDataIntoFile(conversation *GroupMsgRecord, file string) {
--- record/types.go
+++ record/types.go
@@ -1,6 +1,7 @@
 package record
 
 type GroupMsgRecord struct {
+	Time    string
 	Sender  string
 	Content string
 }
